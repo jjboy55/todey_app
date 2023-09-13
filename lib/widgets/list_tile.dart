ed75659c96edf8f3b68d8eb0c09acdab3907a003
jeremiahlouis <114328938+jeremiahlouis@users.noreply.github.com>
2023-09-13 19:57:37 +0100

    final_commit_message

--- lib/widgets/list_tile.dart
+++ lib/widgets/list_tile.dart
@@ -1,19 +1,28 @@
 // ignore_for_file: prefer_const_constructors,prefer_const_constructors_in_immutables, use_key_in_widget_constructors
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todey_app/models/task_data.dart';
 
 class TasksListTile extends StatelessWidget {
   final bool? isChecked;
   late final String taskTitle;
   late final void Function(bool?)? toggleCheckBoxState;
+  late final int index;
 
-  TasksListTile(
-      {required this.isChecked,
-      required this.taskTitle,
-      required this.toggleCheckBoxState});
+  TasksListTile({
+    required this.isChecked,
+    required this.taskTitle,
+    required this.toggleCheckBoxState,
+    required this.index,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+        onLongPress: () {
+          final taskDataReader = context.read<TaskData>();
+          taskDataReader.removeItem(index);
+        },
         title: Text(
           taskTitle,
           style: TextStyle(
